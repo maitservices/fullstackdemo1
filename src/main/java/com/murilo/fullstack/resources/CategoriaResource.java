dad39d9ce7fc05b8db72c81b05a76779bff8a55e
murilo <murilo.a.oliveirati@gmail.com>
2018-09-21 16:00:36 -0300

    domain Categoria, ajuste lista Categoria

--- src/main/java/com/murilo/fullstack/resources/CategoriaResource.java
+++ src/main/java/com/murilo/fullstack/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.murilo.fullstack.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.murilo.fullstack.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest OK";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<Categoria>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
 	}
 
 }
